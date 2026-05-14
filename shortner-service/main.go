fca3f4cbab738752d03e68b611692b24c4682345
MostafaOsama223 <mostafaosama030@gmail.com>
2026-05-14 11:25:32 +0300

    (add) main file

---
+++ shortner-service/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"shortner-service/api"
+	"shortner-service/database"
+	"shortner-service/repo"
+	"shortner-service/service"
+
+	"github.com/joho/godotenv"
+)
+
+func loadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func main() {
+	loadEnv()
+	uri := os.Getenv("MONGO_URI")
+	log.Printf("Starting URL shortener service with MONGO_URI: %s", uri)
+
+	mongodbClient, err := database.NewMongoDB(uri)
+	if err != nil {
+		log.Fatalf("Failed to connect to MongoDB: %v", err)
+	}
+	defer mongodbClient.Disconnect(context.Background())
+
+	db := mongodbClient.Database("url_shortener")
+	log.Printf("Using database: %s", db.Name())
+
+	urlsCollection := mongodbClient.Collection(db, "urls")
+	log.Printf("Using collection: %s", "urls")
+
+	urlRepo := repo.NewUrlRepo(urlsCollection)
+	urlService := service.NewUrlService(urlRepo)
+
+	log.Printf("Starting HTTP server...")
+	urlAPIHandler := api.NewUrlAPIHandler(urlService)
+	urlAPIHandler.Run()
+}
